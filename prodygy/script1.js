b568536907ec758413f8365792a523d4028c5b7a
yamuna <157932558+yamuna130@users.noreply.github.com>
2024-08-25 20:13:14 +0530

    Delete prodygy directory

--- prodygy/script1.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function() {
-    const navbar = document.querySelector('.navbar');
-    if (window.scrollY > 50) {
-        navbar.classList.add('scrolled');
-    } else {
-        navbar.classList.remove('scrolled');
-    }
-});
